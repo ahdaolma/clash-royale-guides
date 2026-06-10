c4ed65f8dc26a1f47e576e07207e58a7fae55156
什么 <3227627182@qq.com>
2026-06-10 17:42:28 +0800

    feat: add Google Search Console verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+      <meta name="google-site-verification" content="Y9bYAWGBOs0iyKHC2knpGdauqdjIi67FhxqLrjKarsM" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6600381860016497" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
